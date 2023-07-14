d99898e43762a1be405df19c81884d7e5bff1eea
tarunpoddar <tarun.tarun05@gmail.com>
2023-07-14 21:44:42 +0530

    Add project files.

---
+++ KafkaDemo.Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ConsumerConfig
+{
+    GroupId = "test-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+
+consumer.Subscribe("my_test_topic");
+
+CancellationTokenSource tokenSource = new();
+
+try
+{
+    while(true)
+    {
+        var resp = consumer.Consume(tokenSource.Token);
+        if(resp != null && resp.Message !=null)
+        {
+            var employee = JsonConvert.DeserializeObject<Employee>
+                (resp.Value);
+            Console.WriteLine($"id: {employee.id}, name: {employee.name}");
+        }
+    }
+}
+catch (Exception)
+{
+	throw;
+}
+
+public record Employee(int id, string name);
---
+++ KafkaDemo.Producer/Program.cs
@@ -0,0 +1,38 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
+
+var producer = new ProducerBuilder<string, string>(config).Build();
+
+try
+{
+    string? input = string.Empty;
+    do
+    {
+        Console.Write("Enter the employee id: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the employee name: ");
+        string name = Console.ReadLine()??"";
+
+        var employee = new Employee(id, name);
+        var response = producer.ProduceAsync("my_test_topic",
+       new Message<string, string>
+       {
+           Key = JsonConvert.SerializeObject(employee.id),
+           Value = JsonConvert.SerializeObject(employee)
+       });
+        Console.WriteLine("Do you want to send more message? Y/n");
+        input = Console.ReadLine();
+    } while (input != null && (input.Contains('y') || input.Contains('Y')));
+}
+catch (ProduceException<Null, string> ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+public record Employee(int id, string name);
